2a1909cf73c85a706a4e9e0620783f659b0c6ceb
fandre04m <andremartins04@gmail.com>
2026-06-03 18:37:19 +0100

    Optimize the main prompt and what the LLM has to gen, hopefully

--- src/constrainer.py
+++ src/constrainer.py
@@ -5,23 +5,22 @@ import time
 class ModelCaller:
     def __init__(self) -> None:
         self.llm = Small_LLM_Model()
-        self.max_tokens: int = 100
+        self.max_tokens: int = 50
         self.elapsed: float = 0.0
 
     def run_prompt(self, prompt: str) -> None:
         res_str = ""
+        start = time.monotonic()
         input_ids = self.llm.encode(prompt)
         ids_lst = input_ids[0].tolist()
         print(len(ids_lst))
         for _ in range(self.max_tokens):
-            start = time.monotonic()
             logits = self.llm.get_logits_from_input_ids(ids_lst)
-            self.elapsed += time.monotonic() - start
-            print(self.elapsed)
             next_token_id = int(logits.index(max(logits)))
             ids_lst.append(next_token_id)
             token = self.llm.decode(next_token_id)
             if token == "<":
                 break
             res_str += token
+        self.elapsed += time.monotonic() - start
         print(res_str)
--- src/prompt_builder.py
+++ src/prompt_builder.py
@@ -11,8 +11,9 @@ class PromptBuilder:
         prompt = [
             "You are function calling system.",
             "",
-            "Your task is to select exactly one function",
-            "and produce ONLY valid JSON.",
+            "Your task is to select exactly one function ",
+            "and it's required arguments.",
+            # "and produce ONLY valid JSON.",
             "",
             "Do not output reasoning.",
             "Do not output prose.",
@@ -23,32 +24,46 @@ class PromptBuilder:
         ]
         for func in functions:
             prompt.append(f"Function: {func.name}")
-            prompt.append(f"Description: {func.description}")
+            # prompt.append(f"Description: {func.description}")
             prompt.append("Arguments:")
             for name, param in func.parameters.items():
                 prompt.append(f" - {name} ({param.type})")
             prompt.append("")
         prompt.extend([
             "Required answer format:",
-            "{",
-            '  "name": "fn_function_name",',
-            '  "parameters": {',
-            '    "arg_name": "value"',
-            "  }",
-            "}",
+            "",
+            "fn_function_name",
+            '"arg_name": value',
+            # "{",
+            # '  "name": "fn_function_name",',
+            # '  "parameters": {',
+            # '    "arg_name": "value"',
+            # "  }",
+            # "}",
             "<END>",
             "",
             "Example user request:",
             "What is the sum of 4 and 7?",
             "",
             "Example answer:",
-            "{",
-            '  "name": "ft_add_numbers",',
-            '  "parameters": {',
-            '    "a": "4",',
-            '    "b": "7"',
-            "  }",
-            "},",
+            "fn_add_numbers",
+            '"a": 4',
+            '"b": 7',
+            # "<END>",
+            # "",
+            # "Example user request:",
+            # "Reverse the string 'hello'",
+            # "",
+            # "Example answer:",
+            # "fn_reverse_string",
+            # '"s": "hello"'
+            # "{",
+            # '  "name": "fn_add_numbers",',
+            # '  "parameters": {',
+            # '    "a": "4",',
+            # '    "b": "7"',
+            # "  }",
+            # "},",
             "<END>",
             "",
             "User request:",
